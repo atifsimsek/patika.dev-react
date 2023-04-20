f8e907c2c7e693a05a465483a5328253918bcc7b
Atıf Şimşek <dev.atifsimsek@gmail.com>
2023-04-20 23:08:28 +0300

    task-6 bugs fixed

--- 06-react-task-6/src/store/services/bookService.ts
+++ 06-react-task-6/src/store/services/bookService.ts
@@ -1,11 +1,12 @@
+import { InputValues } from '@/types/InputValues';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 //Fetch Books
 export const fetchBooks = createAsyncThunk(
   'fetch/books',
-  async ({ search, filter, printType, sortBy }: any) => {
-    // destructing inputValues
+  async ({ search, filter, printType, sortBy }: InputValues) => {
+    // get inputValues
     try {
       const response = await axios(
         `https://www.googleapis.com/books/v1/volumes?q=${search}&filter=${filter}&printType=${printType}&orderBy=${sortBy}&startIndex=0&maxResults=40&key=${process.env.BOOK_API_KEY}`
